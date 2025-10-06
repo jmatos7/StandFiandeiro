7482896ad70227bf9ea90f98a92469a3f2828849
jmatos7 <joaomatosskype@example.com>
2025-10-06 17:35:22 +0100

    debbuging

--- stand-rp/src/components/AddCarForm.tsx
+++ stand-rp/src/components/AddCarForm.tsx
@@ -17,7 +17,7 @@ const AddCarForm: React.FC<AddCarFormProps> = ({ onAddCar }) => {
   const [turbo, setTurbo] = useState(false);
   const [price, setPrice] = useState("");
   const [img, setImg] = useState("");
-  const [sold, setSold] = useState(false);
+  const sold = useState(false);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
